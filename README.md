748d11fdabad6607125300d98970edbfff6db3b4
adriana070319 <62315239+adriana070319@users.noreply.github.com>
2022-10-09 18:32:34 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# GobiernoDigital
-Created with CodeSandbox
+# Adriana Sesento Casas
+
+Para navegar en el sitio es únicamente dándole click al pokemon que desea leer cada detalle como sus habilidades, el tipo de pokemon etc.
+
+En aspectos a mejorar como opinión personal el uso de routing package
+
+## Project setup
+
+```
+yarn install
+```
---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <h1>POKEMON</h1>
+
+    <PokemonList
+      :imageUrl="imageUrl"
+      :apiUrl="apiUrl"
+      @setPokemonUrl="setPokemonUrl"
+    />
+    <PokemonDetail
+      v-if="showDetail"
+      :pokemonUrl="pokemonUrl"
+      :imageUrl="imageUrl"
+      @closeDetail="closeDetail"
+    />
+  </div>
+</template>
+
+<script>
+import PokemonList from "./PokemonList.vue";
+import PokemonDetail from "./PokemonDetail.vue";
+export default {
+  data: () => {
+    return {
+      imageUrl:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",
+      apiUrl: "https://pokeapi.co/api/v2/pokemon/",
+      pokemonUrl: "",
+      showDetail: false,
+    };
+  },
+  components: {
+    PokemonList,
+    PokemonDetail,
+  },
+  methods: {
+    setPokemonUrl(url) {
+      this.pokemonUrl = url;
+      this.showDetail = true;
+    },
+    closeDetail() {
+      this.pokemonUrl = "";
+      this.showDetail = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css?family=Acme");
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 10px;
+  width: calc(100% - 20px);
+  min-height: calc(100vh - 20px);
+  background: radial-gradient(#000000, #ff0000);
+  font-family: "acme", fantasy;
+  font-size: 1rem;
+  font-weight: normal;
+}
+h1 {
+  color: #efefef;
+}
+</style>
